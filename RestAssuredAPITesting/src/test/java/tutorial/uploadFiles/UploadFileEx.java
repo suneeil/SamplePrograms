2638d92e1a8999b293c21e7e2064b51b406d8b09
sunil <suneeilrao@gmail.com>
2018-05-28 10:10:08 +0530

    Working on 'Filters','File Download and verify file size' examples

---
+++ RestAssuredAPITesting/src/test/java/tutorial/uploadFiles/UploadFileEx.java
@@ -0,0 +1,21 @@
+package tutorial.uploadFiles;
+
+import java.io.File;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class UploadFileEx {
+//Using https://www.zamzar.com/
+	//https://developers.zamzar.com/
+	//API KEY: 		e4ee5076b3274de7aa0c92c8c55888025287677e
+	
+	@Test
+	public void uploadFileExample(){
+		File inputFile = new File(System.getProperty("user.dir")+"/src/test/java/tutorial/uploadFiles/SourceGIF.gif");
+		System.out.println(inputFile.length());
+		
+		
+	}
+	
+}
